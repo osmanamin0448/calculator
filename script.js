0cf86e4bf1745916b7dc2fb02951b1dc758c4d0f
osmanamin0448 <osmanamin0448@gmail.com>
2026-08-24 15:03:22 +0000

    Add firstNumber, operator, secondNumber Add operate() to run add(), multiply(), subtract(), divide() base on operator.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let firstNumber,
+    secondNumber,
+    operator;
+
 function add(num1, num2){
   return num1 + num2
 }
-
 function subtract(num1, num2){
   return num1 - num2
 }
@@ -12,4 +15,19 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
   return num1 / num2
+}
+
+function operate(firstNumber, operator, secondNumber){
+  if(operator === "+"){
+   return add(firstNumber, secondNumber)
+  }
+  else if(operator === "-"){
+    return subtract(firstNumber, secondNumber)
+  }
+  else if(operator === "*"){
+    return multiply(firstNumber, secondNumber)
+  }
+  else{
+    return divide(firstNumber, secondNumber)
+  }
 }
